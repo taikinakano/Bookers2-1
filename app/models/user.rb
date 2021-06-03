f8bfbb3cd71313fa9b24a7bbbadb17d8eeb05b34
taikinakano <kabayaki3323@gmail.com>
2021-06-03 10:41:42 +0000

    [Finish]1-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :followers, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
-  has_many :follower_users, through: :followings, source: :follower
-  has_many :followings, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
+  
+  has_many :followers, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :following_users, through: :followers, source: :followed
+  
+  has_many :followings, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :follower_users, through: :followings, source: :follower
 
   def follow(user_id)
     followers.create(followed_id: user_id)
